10e80f831f50371185c2972b28d051a86dfc6265
Luca Spinello <spinello.luca.92@gmail.com>
2017-12-04 15:01:10 +0100

    nothing to declare

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var luca = {
+    name: 'Luca',
+    lastName: 'Spinello',
+    yearOfBirth: 1992,
+    monthOfBirth: 'august',
+    dayOfBirth: 24,
+    countryOfBirth: 'Rivoli',
+    state: 'Italia',
+    province: 'Torino'
+};
